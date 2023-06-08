2232e0a76fd563c1ba685d1f4d27e676b5d2650a
Kale Riley <kalkronline@gmail.com>
2023-06-08 09:47:21 -0700

    stuff from last night

--- src/lib.rs
+++ src/lib.rs
@@ -1,283 +1,173 @@
-use std::str;
-use std::io::Cursor;
-use std::io::Read;
-use std::io::{ Seek, SeekFrom };
+use std::io::{self, Read};
 
-const REDIS_PROTO_STRING: u8=b'$';
-const REDIS_PROTO_LIST: u8=b'*';
-const REDIS_PROTO_INT: u8=b':';
-const REDIS_PROTO_OK: u8=b'+';
-const REDIS_PROTO_ERROR: u8=b'-';
-const REDIS_PROTO_CR: u8=13;
-const REDIS_PROTO_LF: u8=10;
+mod redis_protocol {
+    use super::*;
+    use std::io::{Error, ErrorKind::*};
 
+    use bytes::{Buf, BytesMut};
+    use tokio_util::codec::Decoder;
 
-pub struct RedisProtocol {
-    remaining_data: Vec<u8>,
-}
+    const PROTO_STRING: u8 = b'$';
+    const PROTO_LIST: u8 = b'*';
+    const PROTO_INT: u8 = b':';
+    const PROTO_OK: u8 = b'+';
+    const PROTO_ERROR: u8 = b'-';
+    const PROTO_CRLF: &[u8; 2] = &[0x0d, 0x0a];
 
-impl RedisProtocol {
-    pub fn new() -> Self {
-        RedisProtocol {
-            remaining_data: Vec::new(),
-        }
+    #[derive(Debug)]
+    pub enum RedisValue {
+        String(String),
+        Int(i32),
+        List(Vec<RedisValue>),
+        Ok(String),
+        Error(String),
     }
 
-    pub fn more_data(&mut self, data: Vec<u8>) -> Result<usize, &'static str> {
-        self.remaining_data.extend(data);
-        if self.remaining_data.len() > (1024*1024*512) {
-            // 512mb of unparsable data
-            return Err("protocol error")
-        }
-        return Ok(self.remaining_data.len())
-    }
+    pub struct RedisCodec;
 
-    pub fn consume(&mut self) -> Option<RedisValue> {
-        let mut cursor = Cursor::new(&self.remaining_data[..]);
-        let result = RedisProtocol::read_redis(&mut cursor);
-        
-        if result.is_some() {
-            let pos = cursor.position() as usize;
+    impl Decoder for RedisCodec {
+        type Item = RedisValue;
+        type Error = io::Error;
 
-            self.remaining_data.drain(..pos);
-        }
+        fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+            // obtain a new slice pointing to the source
+            // mut slices have cursor functionality built
+            // into the read implemenation
+            let reader = &mut src.as_ref();
 
-        result
-    }
-
-    fn read_length(cursor: &mut Cursor<&[u8]>) -> Option<i32> {
-        let mut buf = Vec::new();
-        let mut byte = [0; 1];
-        loop {
-            if let Ok(()) = cursor.read_exact(&mut byte) {
-                if byte[0] == REDIS_PROTO_CR {
-                    if let Ok(()) = cursor.read_exact(&mut byte) {
-                        if byte[0] == REDIS_PROTO_LF {
-                            break
-                        }
-                    } else {
-                        return None
-                    }
+            match read_value(reader) {
+                Ok(val) => {
+                    src.advance(src.len() - reader.len());
+                    Ok(Some(val))
                 }
-                buf.extend(&byte);
-            } else {
-                return None
-            }
-        }
-
-        let value: i32 = str::from_utf8(&buf).ok()?.trim().parse().ok()?;
-
-        //println!("read_length: {}", value);
-
-        Some(value)
-    }
-
-    fn remove_crlf(cursor: &mut Cursor<&[u8]>) -> Option<bool> {
-        let mut crlf = [0; 2];
-        if let Ok(()) = cursor.read_exact(&mut crlf) {
-            if crlf[0] == REDIS_PROTO_CR && crlf[1] == REDIS_PROTO_LF {
-                return Some(true)
-            } else {
-                panic!("protocol error - no crlf {:?}", crlf);
-            }
-        }
-
-        None
-    }
-
-    fn read_redis(cursor: &mut Cursor<&[u8]>) -> Option<RedisValue> {
-        let mut buf = [0u8; 1];
-        cursor.read_exact(&mut buf).ok()?;
-        //println!("ENTER RedisProtocol::redis_read <{}>", buf[0]);
-        match buf[0] {
-            REDIS_PROTO_STRING => {
-                //println!("RedisProtocol::redis_read::REDIS_PROTO_STRING");
-                let string = RedisProtocol::read_redis_string(cursor)?;
-                return Some(RedisValue::RedisString(string));
-            },
-            REDIS_PROTO_INT => {
-                //println!("RedisProtocol::redis_read::REDIS_PROTO_INT");
-                let integer = RedisProtocol::read_redis_int(cursor)?;
-                return Some(RedisValue::RedisInt(integer));
-            },
-            REDIS_PROTO_LIST => {
-                //println!("RedisProtocol::redis_read::REDIS_PROTO_LIST");
-                return Some(RedisProtocol::read_redis_list(cursor)?);
-            },
-            REDIS_PROTO_OK => {
-                //println!("RedisProtocol::redis_read::REDIS_PROTO_OK");
-                let string = RedisProtocol::read_redis_generic_crlf_string(cursor)?;
-                return Some(RedisValue::RedisOk(string));
-            }
-            REDIS_PROTO_ERROR => {
-                //println!("RedisProtocol::redis_read::REDIS_PROTO_ERROR");
-                let string = RedisProtocol::read_redis_generic_crlf_string(cursor)?;
-                return Some(RedisValue::RedisError(string));
-            }
-            _ => {
-                panic!("RedisProtocol::redis_read::REDIS_PROTO_ERROR [{:?}]", buf[0]);
+                Err(e) if e.kind() == UnexpectedEof => Ok(None),
+                Err(e) => Err(e),
             }
         }
     }
 
-    fn read_redis_list(cursor: &mut Cursor<&[u8]>) -> Option<RedisValue> {
-        let mut parts = Vec::new();
+    fn read_length(src: &mut &[u8]) -> io::Result<i32> {
+        for i in 0.. {
+            let Some([l, r]) = src.get(i..i+2) else {
+                 return Err(Error::new(UnexpectedEof, ""))
+             };
 
-        let num_parts=RedisProtocol::read_length(cursor)?;
+            if [*l, *r] != *PROTO_CRLF {
+                let value = std::str::from_utf8(&src[..i])
+                    .or(Err(Error::new(
+                        InvalidData,
+                        "len read failed (not a string)",
+                    )))?
+                    .trim()
+                    .parse()
+                    .or(Err(Error::new(
+                        InvalidData,
+                        "len read failed (string not a number)",
+                    )))?;
+                take_vec(src, i + 2)?;
 
-        if num_parts == -1 {
+                return Ok(value);
+            }
+        }
+
+        unreachable!()
+    }
+
+    fn pop_crlf(src: &mut &[u8]) -> io::Result<()> {
+        if take_arr(src)? != *PROTO_CRLF {
+            return Err(Error::new(InvalidData, "expected CRLF"));
+        }
+
+        Ok(())
+    }
+
+    fn read_value(src: &mut &[u8]) -> io::Result<RedisValue> {
+        Ok(match take_u8(src)? {
+            PROTO_STRING => read_redis_string(src)?,
+            PROTO_INT => read_redis_int(src)?,
+            PROTO_LIST => read_redis_list(src)?,
+            PROTO_OK => RedisValue::Ok(read_redis_generic_crlf_string(src)?),
+            PROTO_ERROR => RedisValue::Error(read_redis_generic_crlf_string(src)?),
+            _ => return Err(Error::new(InvalidData, "invalid type")),
+        })
+    }
+
+    fn read_redis_list(src: &mut &[u8]) -> io::Result<RedisValue> {
+        let len = read_length(src)?;
+
+        if len == -1 {
             // null list has "*-1\r\n"
-            return Some(RedisValue::RedisList(parts))
+            return Ok(RedisValue::List(Vec::new()));
         }
 
-        for _ in 0..num_parts {
-            if let Some(v)=RedisProtocol::read_redis(cursor) {
-                parts.push(v);
-            } else {
-                return None
-            }
+        let mut parts = Vec::with_capacity(len as usize);
+        for _ in 0..len {
+            parts.push(read_value(src)?);
         }
 
-        Some(RedisValue::RedisList(parts))
+        Ok(RedisValue::List(parts))
     }
 
-    fn read_redis_string(cursor: &mut Cursor<&[u8]>) -> Option<String> {
-        let string_length=RedisProtocol::read_length(cursor)?;
+    fn read_redis_string(src: &mut &[u8]) -> io::Result<RedisValue> {
+        let string_length = read_length(src)?;
 
         if string_length == -1 {
             // "null" string has "$-1\r\n"
-            return Some("".into());
+            return Ok(RedisValue::String("".into()));
         }
 
-        let mut buf = vec![0; string_length as usize];
-        cursor.read_exact(&mut buf).ok()?;
+        let buf = take_vec(src, string_length as usize)?;
+        pop_crlf(src)?;
 
-        RedisProtocol::remove_crlf(cursor)?;
-    
-        let string = str::from_utf8(&buf).ok()?.trim().to_string();
+        let str = std::str::from_utf8(&buf)
+            .or(Err(Error::new(
+                InvalidData,
+                "string read failed (not a string)",
+            )))?
+            .trim();
 
-        //println!("REDIS_PROTO_STRING [{};{:?}]", string_length, string);
-
-        Some(string)
+        Ok(RedisValue::String(str.into()))
     }
 
-    fn read_redis_int(cursor: &mut Cursor<&[u8]>) -> Option<i32> {
-        let mut buf = Vec::new();
-        let mut byte = [0; 1];
-        loop {
-            if let Ok(()) = cursor.read_exact(&mut byte) {
-                if byte[0] == REDIS_PROTO_CR {
-                    if let Ok(()) = cursor.read_exact(&mut byte) {
-                        if byte[0] == REDIS_PROTO_LF {
-                            break
-                        } else {
-                            return None
-                        }
-                    }
-                } else {
-                    buf.extend(&byte);
-                }
-            } else {
-                return None
+    fn read_redis_int(src: &mut &[u8]) -> io::Result<RedisValue> {
+        Ok(RedisValue::Int(read_length(src)?))
+    }
+
+    fn read_redis_generic_crlf_string(src: &mut &[u8]) -> io::Result<String> {
+        for i in 0.. {
+            let Some([l, r]) = src.get(i..i+2) else {
+                 return Err(Error::new(UnexpectedEof, ""))
+             };
+
+            if [*l, *r] != *PROTO_CRLF {
+                let value = std::str::from_utf8(&src[..i])
+                    .or(Err(Error::new(
+                        InvalidData,
+                        "string read failed (not a string)",
+                    )))?
+                    .trim();
+                let _ = take_vec(src, i + 2);
+
+                return Ok(value.into());
             }
         }
 
-        let value: i32 = str::from_utf8(&buf).ok()?.trim().parse().ok()?;
-        //println!("REDIS_PROTO_INT: {}", value);
-        Some(value)
-    }
-
-    fn read_redis_generic_crlf_string(cursor: &mut Cursor<&[u8]>) -> Option<String> {
-        let mut buf: Vec<u8> = Vec::new();
-        let mut byte = [0; 1];
-        loop {
-            if let Ok(()) = cursor.read_exact(&mut byte) {
-                //println!("RedisProtocol::read_redis_generic_crlf_string: {:?}", byte);
-                if byte[0] == REDIS_PROTO_CR {
-                    if let Ok(()) = cursor.read_exact(&mut byte) {
-                        if byte[0] == REDIS_PROTO_LF {
-                            break;
-                        } else {
-                            return None
-                        }
-                    }
-                } else {
-                    buf.extend(&byte);
-                }
-            } else {
-                return None;
-            }
-        }
-
-        println!("read_redis_crlf BUF:{:?}", buf);
-
-        Some(str::from_utf8(&buf).ok()?.trim().to_string())
+        unreachable!()
     }
 }
 
-pub enum RedisValue {
-    RedisString(String),
-    RedisInt(i32),
-    RedisList(Vec<RedisValue>),
-    RedisOk(String),
-    RedisError(String)
+fn take_arr<const N: usize>(src: &mut impl Read) -> io::Result<[u8; N]> {
+    let mut buf = [0; N];
+    src.read_exact(&mut buf)?;
+    Ok(buf)
 }
 
-impl RedisValue {
-    fn debugprint(&self) {
-        self._debugprint(0)
-    }
-
-    fn _debugprint(&self, depth: usize) {
-        print!("{}", std::iter::repeat("  ").take(depth).collect::<String>());
-        match self {
-            RedisValue::RedisString(value) => println!("RedisString: {};{}", value.len(), value),
-            RedisValue::RedisInt(value) => println!("RedisInt: {}", value),
-            RedisValue::RedisList(value) => {
-                println!("RedisList: {}", value.len());
-                for item in value {
-                    item._debugprint(depth + 1 as usize);
-                }
-            }
-            RedisValue::RedisOk(value) => println!("OK! {}", value),
-            RedisValue::RedisError(value) => println!("ERROR! {}", value),
-        }
-    }
+fn take_vec(src: &mut impl Read, n: usize) -> io::Result<Vec<u8>> {
+    let mut buf = vec![0_u8; n];
+    src.read_exact(&mut buf)?;
+    Ok(buf)
 }
 
-fn main() {
-    let data=include_bytes!("proto_traffic.bin");
-    
-    let mut protocol = RedisProtocol::new();
-
-    // example subscribe
-    // *2
-    // $9
-    // subscribe
-    // $27    <--- length of string below
-    // groupbroadcast::control::46        <---- channel name
-
-    //protocol.more_data(data.to_vec());
-
-    // // test the whole stream at once
-    // loop {
-    //     if let Some(cmd)=protocol.consume() {
-    //         cmd.debugprint();
-    //     } else {
-    //         break;
-    //     }
-    // }
-
-    // test one byte at a time
-    for byte in data.iter() {
-        protocol.more_data(vec![*byte]);
-        //println!("append more data {}", protocol.more_data(vec![*byte]).expect("err".into()));
-        loop {
-            if let Some(cmd)=protocol.consume() {
-                cmd.debugprint();
-            } else {
-                break;
-            }
-        }
-    }
+fn take_u8(src: &mut impl Read) -> io::Result<u8> {
+    take_arr::<1>(src).map(|[x]| x)
 }
