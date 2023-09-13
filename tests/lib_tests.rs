69ea5eccd3c01c2294d24c535138287aa5cd6422
Chris Given <chris.given@moonlightcompanies.com>
2023-09-13 15:43:35 -0700

    single channel, helper FNs

--- tests/lib_tests.rs
+++ tests/lib_tests.rs
@@ -21,16 +21,34 @@ fn test_generic(data: &[u8], expected: Vec<Box<RespValue>>) {
     }
 }
 
+fn help_create_bulkstring(txt: &str) -> RespValue {
+    RespValue::BulkString(Some(Box::from(txt.as_bytes().to_vec())))
+}
+
+fn help_create_simplestring(txt: &str) -> RespValue {
+    RespValue::SimpleString(Box::from(txt.to_string()))
+}
+
+fn help_create_integer(num: i64) -> RespValue {
+    RespValue::Integer(num)
+}
+
+fn help_create_array(values: Vec<RespValue>) -> RespValue {
+    RespValue::Array(Some(values))
+}
+
 #[test]
 fn test_ping_simple() {
     let rx_bytes = include_bytes!("../example_test_cases/ping_simple/Rx.bin");
     let tx_bytes = include_bytes!("../example_test_cases/ping_simple/Tx.bin");
 
-    test_generic(&rx_bytes[..], vec![Box::new(RespValue::SimpleString(Box::from("PONG".to_string())))]);
+    test_generic(&rx_bytes[..], vec![Box::new(
+        help_create_simplestring("PONG")
+    )]);
     
-    test_generic(&tx_bytes[..], vec![Box::new(RespValue::Array(Some(vec![
-        RespValue::BulkString(Some(Box::from("ping".as_bytes().to_vec()))),
-    ])))]);
+    test_generic(&tx_bytes[..], vec![Box::new(help_create_array(vec![
+        help_create_bulkstring("ping"),
+    ]))]);
 }
 
 #[test]
@@ -38,12 +56,12 @@ fn test_ping_bulk() {
     let rx_bytes = include_bytes!("../example_test_cases/ping_bulk/Rx.bin");
     let tx_bytes = include_bytes!("../example_test_cases/ping_bulk/Tx.bin");
 
-    test_generic(&rx_bytes[..], vec![Box::new(RespValue::BulkString(Some(Box::from("hello world".as_bytes().to_vec()))))]);
+    test_generic(&rx_bytes[..], vec![Box::new(help_create_bulkstring("hello world"))]);
     
-    test_generic(&tx_bytes[..], vec![Box::new(RespValue::Array(Some(vec![
-        RespValue::BulkString(Some(Box::from("ping".as_bytes().to_vec()))),
-        RespValue::BulkString(Some(Box::from("hello world".as_bytes().to_vec()))),
-    ])))]);
+    test_generic(&tx_bytes[..], vec![Box::new(help_create_array(vec![
+        help_create_bulkstring("ping"),
+        help_create_bulkstring("hello world"),
+    ]))]);
 }
 
 #[test]
@@ -51,16 +69,16 @@ fn test_subscribe_single_channel() {
     let rx_bytes = include_bytes!("../example_test_cases/subscribe_single_channel/Rx.bin");
     let tx_bytes = include_bytes!("../example_test_cases/subscribe_single_channel/Tx.bin");
 
-    test_generic(&rx_bytes[..], vec![Box::new(RespValue::Array(Some(vec![
-        RespValue::BulkString(Some(Box::from("subscribe".as_bytes().to_vec()))),
-        RespValue::BulkString(Some(Box::from("test_channel_1".as_bytes().to_vec()))),
-        RespValue::Integer(1),
-    ])))]);
+    test_generic(&rx_bytes[..], vec![Box::new(help_create_array(vec![
+        help_create_bulkstring("subscribe"),
+        help_create_bulkstring("test_channel_1"),
+        help_create_integer(1),
+    ]))]);
 
-    test_generic(&tx_bytes[..], vec![Box::new(RespValue::Array(Some(vec![
-        RespValue::BulkString(Some(Box::from("subscribe".as_bytes().to_vec()))),
-        RespValue::BulkString(Some(Box::from("test_channel_1".as_bytes().to_vec()))),
-    ])))]);
+    test_generic(&tx_bytes[..], vec![Box::new(help_create_array(vec![
+        help_create_bulkstring("subscribe"),
+        help_create_bulkstring("test_channel_1"),
+    ]))]);
 }
 
 
@@ -69,24 +87,24 @@ fn test_subscribe_multiple_channels() {
     let rx_bytes = include_bytes!("../example_test_cases/subscribe_multiple_channels/Rx.bin");
     let tx_bytes = include_bytes!("../example_test_cases/subscribe_multiple_channels/Tx.bin");
 
-    test_generic(&rx_bytes[..], vec![Box::new(RespValue::Array(Some(vec![
-        RespValue::BulkString(Some(Box::from("subscribe".as_bytes().to_vec()))),
-        RespValue::BulkString(Some(Box::from("test_channel_1".as_bytes().to_vec()))),
-        RespValue::Integer(1),
-    ]))), Box::new(RespValue::Array(Some(vec![
-        RespValue::BulkString(Some(Box::from("subscribe".as_bytes().to_vec()))),
-        RespValue::BulkString(Some(Box::from("test_channel_2".as_bytes().to_vec()))),
-        RespValue::Integer(2),
-    ]))), Box::new(RespValue::Array(Some(vec![
-        RespValue::BulkString(Some(Box::from("subscribe".as_bytes().to_vec()))),
-        RespValue::BulkString(Some(Box::from("test_channel_3".as_bytes().to_vec()))),
-        RespValue::Integer(3),
-    ])))]);
+    test_generic(&rx_bytes[..], vec![Box::new(help_create_array(vec![
+        help_create_bulkstring("subscribe"),
+        help_create_bulkstring("test_channel_1"),
+        help_create_integer(1),
+    ])), Box::new(help_create_array(vec![
+        help_create_bulkstring("subscribe"),
+        help_create_bulkstring("test_channel_2"),
+        help_create_integer(2),
+    ])), Box::new(help_create_array(vec![
+        help_create_bulkstring("subscribe"),
+        help_create_bulkstring("test_channel_3"),
+        help_create_integer(3),
+    ]))]);
     
     test_generic(&tx_bytes[..], vec![Box::new(RespValue::Array(Some(vec![
-        RespValue::BulkString(Some(Box::from("subscribe".as_bytes().to_vec()))),
-        RespValue::BulkString(Some(Box::from("test_channel_1".as_bytes().to_vec()))),
-        RespValue::BulkString(Some(Box::from("test_channel_2".as_bytes().to_vec()))),
-        RespValue::BulkString(Some(Box::from("test_channel_3".as_bytes().to_vec()))),
+        help_create_bulkstring("subscribe"),
+        help_create_bulkstring("test_channel_1"),
+        help_create_bulkstring("test_channel_2"),
+        help_create_bulkstring("test_channel_3"),
     ])))]);
 }
