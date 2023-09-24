1ea9eb868008316ffa3e0fbc0e73fe3bd604e80a
Chris Given <chris.given@moonlightcompanies.com>
2023-09-24 16:44:53 -0700

    use macro for more elaborate test that encodes, decodes --  encode -> validate -> decode -> validate

--- tests/lib_encode.rs
+++ tests/lib_encode.rs
@@ -2,70 +2,52 @@ use redis_proto_parse::resp::{value, RespCodec, encoder};
 use tokio_util::codec::Decoder;
 use bytes::BytesMut;
 
+#[macro_export]
+macro_rules! test_encode_decode {
+    ($encodefn:expr, $encodeval:expr, $encodestr:expr) => {
+        {
+            let input=$encodefn($encodeval);
+            let mut data = BytesMut::new();
+            encoder::resp_encode(input, &mut data);
+            assert_eq!(data, BytesMut::from($encodestr));
+
+            let mut codec = RespCodec::default();
+            match codec.decode(&mut data) {
+                Ok(Some(resp_value)) => {
+                    assert_eq!(resp_value, $encodefn($encodeval));
+                }
+                Ok(None) => {
+                    panic!("Unexpected EOF");
+                }
+                Err(e) => {
+                    panic!("An error occurred: {:?}", e);
+                }
+            }
+        }
+    };
+}
+
 #[test]
 fn test_simple_encode_value_bulk() {
-    let input=value::bulk("bulk");
-    let mut data = BytesMut::new();
-
-    encoder::resp_encode(input, &mut data);
-
-    assert_eq!(data, BytesMut::from("$4\r\nbulk\r\n"));
+    test_encode_decode!(value::bulk, "bulk", "$4\r\nbulk\r\n")
 }
 
 #[test]
 fn test_simple_encode_value_int() {
-    let input=value::int(42);
-    let mut data = BytesMut::new();
-
-    encoder::resp_encode(input, &mut data);
-
-    assert_eq!(data, BytesMut::from(":42\r\n"));
+    test_encode_decode!(value::int, 42, ":42\r\n")
 }
 
 #[test]
 fn test_simple_encode_value_simple() {
-    let input=value::simple("test");
-    let mut data = BytesMut::new();
-
-    encoder::resp_encode(input, &mut data);
-
-    assert_eq!(data, BytesMut::from("+test\r\n"));
+    test_encode_decode!(value::simple, "pass", "+pass\r\n")
 }
 
 #[test]
 fn test_simple_encode_value_err() {
-    let input=value::err("fail");
-    let mut data = BytesMut::new();
-
-    encoder::resp_encode(input, &mut data);
-
-    assert_eq!(data, BytesMut::from("-fail\r\n"));
+    test_encode_decode!(value::err, "fail", "-fail\r\n")
 }
 
 #[test]
 fn test_simple_encode_value_array() {
-    let input=value::array(vec![]);
-    let mut data = BytesMut::new();
-
-    encoder::resp_encode(input, &mut data);
-
-    assert_eq!(data, BytesMut::from("*0\r\n"));
-}
-
-#[test]
-fn test_simple_encode_values() {
-    // test to make sure encoded values produce the expected results in basic cases
-    // lots of other tests use encode on the input and output side of tests
-    let v=value::array(vec![
-        value::bulk("bulk"),
-        value::int(1),
-        value::simple("foo"),
-        value::err("bar"),
-    ]);
-
-    let mut data = BytesMut::new();
-
-    encoder::resp_encode(v, &mut data);
-
-    assert_eq!(data, BytesMut::from("*4\r\n$4\r\nbulk\r\n:1\r\n+foo\r\n-bar\r\n"));
+    test_encode_decode!(value::array, vec![], "*0\r\n")
 }
